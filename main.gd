baff7eee74173e3ad460fc0551e638479c94c0d0
Zalmoxes <zach@tamz.net>
2025-04-04 20:15:38 -0400

    Input
    Added an input action to change text color

--- main.gd
+++ main.gd
@@ -5,3 +5,10 @@ extends Node
 func _ready():
 	$Label.text = "Hello, world!"
 	$Label.modulate = Color.GREEN
+	
+func _input(event):
+	if event.is_action_pressed("my_action"):
+		$Label.modulate = Color.RED
+		
+	if event.is_action_released("my_action"):
+		$Label.modulate = Color.GREEN
