47f662c94787106403911b28220adda7c7e497c7
Zalmoxes <zach@tamz.net>
2025-04-08 12:29:35 -0400

    If-statements

--- main.gd
+++ main.gd
@@ -2,7 +2,6 @@ extends Node
 
 var health = 100
 
-# Called when the node enters the scene tree for the first time.
 func _input(event):
 	if event.is_action_pressed("my_action"):
 		health -=  20
@@ -11,4 +10,7 @@ func _input(event):
 		if health <= 0:
 			health = 0
 			print("YOU DIED!")
-
+		elif health < 50:
+			print("YOU ARE INJURED!")
+		else:
+			print("YOU ARE HEALTHY.")
