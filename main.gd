b21b1388973b9185355d1ecb109bcfb4c13f239f
Zalmoxes <zach@tamz.net>
2025-04-08 12:36:41 -0400

    Variables 2.0
    Went over all types of variables and data structures used in Godot, ended on const variables.

--- main.gd
+++ main.gd
@@ -1,7 +1,6 @@
 extends Node
 
+const damage := 15
+
 func _ready():
-	# This is a comment about the code that follows
-	#print("Hello, world!") # This is a comment about this line only
-	#print("I'm not sure about this...")
 	pass
