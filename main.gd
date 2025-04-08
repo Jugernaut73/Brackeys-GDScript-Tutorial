8621af7b74d37de32c9a0af532e9a743c4f8b01d
Zalmoxes <zach@tamz.net>
2025-04-08 13:11:01 -0400

    Loops

--- main.gd
+++ main.gd
@@ -1,10 +1,13 @@
 extends Node
 
 func _ready():
-	var items: Array[String] = ["Potion", "Feather", "Stolen harp"]
+	var glass := 0.0
 	
-	items[1] = "Smelly sock"
-	items[2] = "Staff"
+	while glass < 0.5:
+		glass += randf_range(0.01, 0.2)
+		
+		if glass > 0.2:
+			break
+		print(glass)
 
-	items.remove_at(1)
-	items.append("Overpowered sword")
+	print("The glass is now half full!")
