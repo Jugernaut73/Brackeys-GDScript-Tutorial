8954ee63e540c545336db023c04e490d72c6eabf
Zalmoxes <zach@tamz.net>
2025-04-08 13:16:25 -0400

    Dictionaries

--- main.gd
+++ main.gd
@@ -1,13 +1,10 @@
 extends Node
 
 func _ready():
-	var glass := 0.0
-	
-	while glass < 0.5:
-		glass += randf_range(0.01, 0.2)
-		
-		if glass > 0.2:
-			break
-		print(glass)
+	var players = {
+			"Crook": 	{"Level": 1, "Health": 80}, 
+			"Villian": 	{"Level": 50, "Health": 150}, 
+			"Boss": 	{"Level": 100, "Health": 500}
+		}
 
-	print("The glass is now half full!")
+	print(players["Boss"]["Health"])
