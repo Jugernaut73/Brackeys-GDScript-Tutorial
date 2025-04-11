5cde380b9ef3620be667cce6cb66391c6dfa0a55
Zalmoxes <zach@tamz.net>
2025-04-11 12:53:52 -0400

    Modifying Nodes 2.0

--- main.gd
+++ main.gd
@@ -1,16 +1,9 @@
 extends Node
 
-enum Alignment { ALLY = 1, NEUTRAL = 0, ENEMY = -1 }
-
-@export var unit_alignment : Alignment
+@onready var player = get_node("Player")
+@export var my_node: Sprite2D
 
 func _ready():
-	match unit_alignment:
-		Alignment.ALLY:
-			print("Hello, friend!")
-		Alignment.NEUTRAL:
-			print("I come in peace!")
-		Alignment.ENEMY:
-			print("TASTE MY WRATH!")
-		_:
-			print("Who art thou?")
+	if my_node is Node2D:
+		print("Is 2D!")
+	print(player.get_path())
